3291296381cf7bd4b80237505e52af1030c45fb3
Quoc bao <baobondev@gmail.com>
2023-03-23 14:37:23 +0700

    Fixed phoneController

--- lib/src/app_widgets/app_text_form_field.dart
+++ lib/src/app_widgets/app_text_form_field.dart
@@ -2,7 +2,7 @@ part of app_widget;
 
 class AppTextFormField extends StatelessWidget {
   final Widget? prefixIcon;
-  final bool obscureText;
+  final bool? obscureText;
   final String hintText;
   final TextEditingController controller;
   final String? errorText;
@@ -11,36 +11,44 @@ class AppTextFormField extends StatelessWidget {
       {super.key,
       this.prefixIcon,
       required this.hintText,
-      this.obscureText = false, this.errorText, required this.controller});
+      this.obscureText = false,
+      this.errorText,
+      required this.controller});
 
   const AppTextFormField.password({
     super.key,
-    required this.hintText,  this.errorText, required this.controller,
+    required this.hintText,
+    this.errorText,
+    required this.controller,
   })  : obscureText = true,
-        prefixIcon = errorText == null ?  const AppIcons.password() : const AppIcons.password(color: Colors.redAccent);
-
+        prefixIcon = errorText == null
+            ? const AppIcons.password()
+            : const AppIcons.password(color: Colors.redAccent);
 
   const AppTextFormField.email({
     super.key,
-    required this.hintText, this.errorText, required this.controller,
+    required this.hintText,
+    this.errorText,
+    required this.controller,
   })  : obscureText = false,
-  prefixIcon = errorText == null ?  const AppIcons.email() : const AppIcons.email(color: Colors.redAccent);
-
+        prefixIcon = errorText == null
+            ? const AppIcons.email()
+            : const AppIcons.email(color: Colors.redAccent);
 
   const AppTextFormField.phone({
     super.key,
-    required this.hintText,  this.errorText, required this.controller,
+    required this.hintText,
+    this.errorText,
+    required this.controller,
   })  : obscureText = false,
         prefixIcon = null;
-
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      obscureText: obscureText,
+      obscureText: obscureText ?? false,
       style: Theme.of(context).textTheme.bodyMedium,
-
       decoration: InputDecoration(
         hintStyle: Theme.of(context).textTheme.labelMedium,
         hintText: hintText,
