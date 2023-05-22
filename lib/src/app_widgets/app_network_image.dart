424a5b79d4b541db4476d785274596653c99a477
BaoBui <bao.bui@fastboy.net>
2023-05-22 11:49:03 +0700

    enableDarkMode

--- lib/src/app_widgets/app_network_image.dart
+++ lib/src/app_widgets/app_network_image.dart
@@ -6,12 +6,14 @@ class AppNetworkImage extends StatelessWidget {
   final double? width;
   final double? height;
   final double borderRadius;
+  final bool enableDarkMode;
 
   const AppNetworkImage(
       {Key? key,
       required this.url,
       this.fit,
       this.width,
+      this.enableDarkMode = false,
       this.height,
       this.borderRadius = 0})
       : super(key: key);
@@ -28,7 +30,15 @@ class AppNetworkImage extends StatelessWidget {
           imageBuilder: (context, imageProvider) => DecoratedBox(
               decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: imageProvider, fit: fit ?? BoxFit.cover))),
+            image: imageProvider,
+            fit: fit ?? BoxFit.cover,
+            colorFilter: enableDarkMode
+                ? ColorFilter.mode(
+                    Colors.black.withOpacity(0.6),
+                    BlendMode.darken,
+                  )
+                : null,
+          ))),
           placeholder: (context, url) => const AppLoading(isLoading: true),
           errorWidget: (context, url, error) =>
               Icon(Icons.error, color: Theme.of(context).colorScheme.onError),
